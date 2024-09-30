f3714729b2ac3836cb34b0c3808f0d12daee8b6c
Nikola Bulat <nbulat99@gmail.com>
2024-09-30 10:15:33 +0200

    Add the configurations and scripts for the size generalization experiments

---
+++ experiments/table_1_euclidean_size_generalization.sh
@@ -0,0 +1,10 @@
+python experiments/train_synthetic.py --config experiments/configs/synth_euclidean_size_generalization.yaml --log_timestamp table1 --embed_dim 3
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_euclidean_size_generalization.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 3
+python experiments/train_synthetic.py --config experiments/configs/synth_euclidean_size_generalization.yaml --log_timestamp table1 --embed_dim 5
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_euclidean_size_generalization.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 5
+python experiments/train_synthetic.py --config experiments/configs/synth_euclidean_size_generalization.yaml --log_timestamp table1 --embed_dim 10
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_euclidean_size_generalization.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 10
+python experiments/train_synthetic.py --config experiments/configs/synth_euclidean_size_generalization.yaml --log_timestamp table1 --embed_dim 20
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_euclidean_size_generalization.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 20
+python experiments/train_synthetic.py --config experiments/configs/synth_euclidean_size_generalization.yaml --log_timestamp table1 --embed_dim 256
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_euclidean_size_generalization.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 256
