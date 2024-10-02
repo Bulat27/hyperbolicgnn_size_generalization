b76d81527526bbfc341b53ca35718a5bd9e9b896
Nikola Bulat <nbulat99@gmail.com>
2024-10-02 13:09:16 +0200

    Add the script to evaluate size generalization of a trained model on a new test dataset

---
+++ experiments/size_gen_eval.sh
@@ -0,0 +1,17 @@
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_euclidean_size_generalization.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 3
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_euclidean_size_generalization.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 5
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_euclidean_size_generalization.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 10
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_euclidean_size_generalization.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 20
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_euclidean_size_generalization.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 256
+
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_lorentz_size_generalization.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 3
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_lorentz_size_generalization.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 5
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_lorentz_size_generalization.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 10
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_lorentz_size_generalization.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 20
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_lorentz_size_generalization.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 256
+
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_poincare_size_generalization.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 3
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_poincare_size_generalization.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 5
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_poincare_size_generalization.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 10
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_poincare_size_generalization.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 20
+python experiments/evaluate_synthetic.py --config experiments/configs/synth_poincare_size_generalization.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 256
