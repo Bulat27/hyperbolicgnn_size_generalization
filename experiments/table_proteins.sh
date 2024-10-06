fdd4cfd54af948d608f2daa1f0f4bfa292993324
Nikola Bulat <nbulat99@gmail.com>
2024-10-06 10:34:34 +0200

    Prepare the script for the PROTEINS dataset experiments

--- experiments/table_proteins.sh
+++ experiments/table_proteins.sh
@@ -1,32 +1,36 @@
-python experiments/train_synthetic.py --config experiments/configs/proteins_euclidean.yaml --log_timestamp table1 --embed_dim 3 --dataset "proteins"
-python experiments/evaluate_synthetic.py --config experiments/configs/proteins_euclidean.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 3 --dataset "proteins"
-python experiments/train_synthetic.py --config experiments/configs/proteins_euclidean.yaml --log_timestamp table1 --embed_dim 5 --dataset "proteins"
-python experiments/evaluate_synthetic.py --config experiments/configs/proteins_euclidean.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 5 --dataset "proteins"
-python experiments/train_synthetic.py --config experiments/configs/proteins_euclidean.yaml --log_timestamp table1 --embed_dim 10 --dataset "proteins"
-python experiments/evaluate_synthetic.py --config experiments/configs/proteins_euclidean.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 10 --dataset "proteins"
-python experiments/train_synthetic.py --config experiments/configs/proteins_euclidean.yaml --log_timestamp table1 --embed_dim 20 --dataset "proteins"
-python experiments/evaluate_synthetic.py --config experiments/configs/proteins_euclidean.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 20 --dataset "proteins"
-python experiments/train_synthetic.py --config experiments/configs/proteins_euclidean.yaml --log_timestamp table1 --embed_dim 256 --dataset "proteins"
-python experiments/evaluate_synthetic.py --config experiments/configs/proteins_euclidean.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 256 --dataset "proteins"
+SEED=$(date +%s)
+echo $SEED >> random_seeds.txt
+echo "Using seed: $SEED"
 
-python experiments/train_synthetic.py --config experiments/configs/proteins_poincare.yaml --log_timestamp table1 --embed_dim 3 --dataset "proteins"
-python experiments/evaluate_synthetic.py --config experiments/configs/proteins_poincare.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 3 --dataset "proteins"
-python experiments/train_synthetic.py --config experiments/configs/proteins_poincare.yaml --log_timestamp table1 --embed_dim 5 --dataset "proteins"
-python experiments/evaluate_synthetic.py --config experiments/configs/proteins_poincare.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 5 --dataset "proteins"
-python experiments/train_synthetic.py --config experiments/configs/proteins_poincare.yaml --log_timestamp table1 --embed_dim 10 --dataset "proteins"
-python experiments/evaluate_synthetic.py --config experiments/configs/proteins_poincare.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 10 --dataset "proteins"
-python experiments/train_synthetic.py --config experiments/configs/proteins_poincare.yaml --log_timestamp table1 --embed_dim 20 --dataset "proteins"
-python experiments/evaluate_synthetic.py --config experiments/configs/proteins_poincare.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 20 --dataset "proteins"
-python experiments/train_synthetic.py --config experiments/configs/proteins_poincare.yaml --log_timestamp table1 --embed_dim 256 --dataset "proteins"
-python experiments/evaluate_synthetic.py --config experiments/configs/proteins_poincare.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 256 --dataset "proteins"
+python experiments/train_synthetic.py --config experiments/configs/proteins_euclidean.yaml --log_timestamp table1 --embed_dim 3 --dataset "proteins" --seed $SEED
+python experiments/evaluate_synthetic.py --config experiments/configs/proteins_euclidean.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 3 --dataset "proteins" --seed $SEED
+python experiments/train_synthetic.py --config experiments/configs/proteins_euclidean.yaml --log_timestamp table1 --embed_dim 5 --dataset "proteins" --seed $SEED
+python experiments/evaluate_synthetic.py --config experiments/configs/proteins_euclidean.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 5 --dataset "proteins" --seed $SEED
+python experiments/train_synthetic.py --config experiments/configs/proteins_euclidean.yaml --log_timestamp table1 --embed_dim 10 --dataset "proteins" --seed $SEED
+python experiments/evaluate_synthetic.py --config experiments/configs/proteins_euclidean.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 10 --dataset "proteins" --seed $SEED
+python experiments/train_synthetic.py --config experiments/configs/proteins_euclidean.yaml --log_timestamp table1 --embed_dim 20 --dataset "proteins" --seed $SEED
+python experiments/evaluate_synthetic.py --config experiments/configs/proteins_euclidean.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 20 --dataset "proteins" --seed $SEED
+python experiments/train_synthetic.py --config experiments/configs/proteins_euclidean.yaml --log_timestamp table1 --embed_dim 256 --dataset "proteins" --seed $SEED
+python experiments/evaluate_synthetic.py --config experiments/configs/proteins_euclidean.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 256 --dataset "proteins" --seed $SEED
 
-python experiments/train_synthetic.py --config experiments/configs/proteins_lorentz.yaml --log_timestamp table1 --embed_dim 3 --dataset "proteins"
-python experiments/evaluate_synthetic.py --config experiments/configs/proteins_lorentz.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 3 --dataset "proteins"
-python experiments/train_synthetic.py --config experiments/configs/proteins_lorentz.yaml --log_timestamp table1 --embed_dim 5 --dataset "proteins"
-python experiments/evaluate_synthetic.py --config experiments/configs/proteins_lorentz.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 5 --dataset "proteins"
-python experiments/train_synthetic.py --config experiments/configs/proteins_lorentz.yaml --log_timestamp table1 --embed_dim 10 --dataset "proteins"
-python experiments/evaluate_synthetic.py --config experiments/configs/proteins_lorentz.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 10 --dataset "proteins"
-python experiments/train_synthetic.py --config experiments/configs/proteins_lorentz.yaml --log_timestamp table1 --embed_dim 20 --dataset "proteins"
-python experiments/evaluate_synthetic.py --config experiments/configs/proteins_lorentz.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 20 --dataset "proteins"
-python experiments/train_synthetic.py --config experiments/configs/proteins_lorentz.yaml --log_timestamp table1 --embed_dim 256 --dataset "proteins"
-python experiments/evaluate_synthetic.py --config experiments/configs/proteins_lorentz.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 256 --dataset "proteins"
+python experiments/train_synthetic.py --config experiments/configs/proteins_poincare.yaml --log_timestamp table1 --embed_dim 3 --dataset "proteins" --seed $SEED
+python experiments/evaluate_synthetic.py --config experiments/configs/proteins_poincare.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 3 --dataset "proteins" --seed $SEED
+python experiments/train_synthetic.py --config experiments/configs/proteins_poincare.yaml --log_timestamp table1 --embed_dim 5 --dataset "proteins" --seed $SEED
+python experiments/evaluate_synthetic.py --config experiments/configs/proteins_poincare.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 5 --dataset "proteins" --seed $SEED
+python experiments/train_synthetic.py --config experiments/configs/proteins_poincare.yaml --log_timestamp table1 --embed_dim 10 --dataset "proteins" --seed $SEED
+python experiments/evaluate_synthetic.py --config experiments/configs/proteins_poincare.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 10 --dataset "proteins" --seed $SEED
+python experiments/train_synthetic.py --config experiments/configs/proteins_poincare.yaml --log_timestamp table1 --embed_dim 20 --dataset "proteins" --seed $SEED
+python experiments/evaluate_synthetic.py --config experiments/configs/proteins_poincare.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 20 --dataset "proteins" --seed $SEED
+python experiments/train_synthetic.py --config experiments/configs/proteins_poincare.yaml --log_timestamp table1 --embed_dim 256 --dataset "proteins" --seed $SEED
+python experiments/evaluate_synthetic.py --config experiments/configs/proteins_poincare.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 256 --dataset "proteins" --seed $SEED
+
+python experiments/train_synthetic.py --config experiments/configs/proteins_lorentz.yaml --log_timestamp table1 --embed_dim 3 --dataset "proteins" --seed $SEED
+python experiments/evaluate_synthetic.py --config experiments/configs/proteins_lorentz.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 3 --dataset "proteins" --seed $SEED
+python experiments/train_synthetic.py --config experiments/configs/proteins_lorentz.yaml --log_timestamp table1 --embed_dim 5 --dataset "proteins" --seed $SEED
+python experiments/evaluate_synthetic.py --config experiments/configs/proteins_lorentz.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 5 --dataset "proteins" --seed $SEED
+python experiments/train_synthetic.py --config experiments/configs/proteins_lorentz.yaml --log_timestamp table1 --embed_dim 10 --dataset "proteins" --seed $SEED
+python experiments/evaluate_synthetic.py --config experiments/configs/proteins_lorentz.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 10 --dataset "proteins" --seed $SEED
+python experiments/train_synthetic.py --config experiments/configs/proteins_lorentz.yaml --log_timestamp table1 --embed_dim 20 --dataset "proteins" --seed $SEED
+python experiments/evaluate_synthetic.py --config experiments/configs/proteins_lorentz.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 20 --dataset "proteins" --seed $SEED
+python experiments/train_synthetic.py --config experiments/configs/proteins_lorentz.yaml --log_timestamp table1 --embed_dim 256 --dataset "proteins" --seed $SEED
+python experiments/evaluate_synthetic.py --config experiments/configs/proteins_lorentz.yaml --csv_file table1.csv --log_timestamp table1 --embed_dim 256 --dataset "proteins" --seed $SEED
